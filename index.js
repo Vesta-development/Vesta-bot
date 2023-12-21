8c81bf05a08b946a59297a90d55e2269e93bf7c8
GeantW0rld <yanis.geantworld@gmail.com>
2023-12-21 20:22:08 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const { Client, GatewayIntentBits, IntentsBitField, Partials } = require("discord.js")
+const { Channel, GuildMember, Message, Reaction, ThreadMember, User, GuildScheduledEvent } = Partials
+require("dotenv").config()
+
+
+const client = new Client({
+    intents: 131071,
+    partials: [Channel, GuildMember, Message, Reaction, ThreadMember, User, GuildScheduledEvent]
+})
+
+client.login(process.env.token).then(async (b) => {
+    
+})
